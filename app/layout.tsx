e0196a0b67ec7cb9a5cfb4b5c0b65585bf4652fa
joakinetm-rgb <joakine.tm@gmail.com>
2025-10-06 23:56:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 
 import './globals.css';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
+import Header from '../components/header';
+import Footer from '../components/footer';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
